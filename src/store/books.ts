a7987fdc5ec404f9da98ff768c3b2493bba2b171
vhetet <vh1030478@gmail.com>
2018-07-25 16:01:40 -0500

    replacing mock data by data pulled from the API

--- src/store/books.ts
+++ src/store/books.ts
@@ -2,34 +2,14 @@ import BookApi from '../bookApi/BookApi';
 import IBook from '../model/Book'
 import * as ActionTypes from './Actions';
 
-const initialState : IBook[] = [
-    {
-        author:'JRR Martin',
-        id: 0,
-        name:'A song of Ice and Fire',
-        page: 700,
-        price: 12.20
-    },
-    {
-        author:'Tolkien',
-        id: 1,
-        name:'The lord of the ring',
-        page: 700,
-        price: 12.20
-    },
-    {
-        author:'JK Rowling',
-        id: 2,
-        name:'Harry Potter',
-        page: 700,
-        price: 12.20
-    }
-]
+const initialState : IBook[] = [];
 
 const books = (state = initialState, action: any) => {
     switch(action.type) {
         case ActionTypes.DELETE_BOOK:
             return state.filter(book => book.id !== action.bookId)
+        case ActionTypes.LOAD_BOOKS_SUCCESS:
+            return state = action.bookArray;
         default:
             return state;
     }
@@ -42,7 +22,7 @@ export interface IState {
 }
 
 export function loadBooksSuccess(bookArray: IBook[]) {
-    return { type: 'LOAD_BOOKS_SUCCESS', bookArray }
+    return { type: ActionTypes.LOAD_BOOKS_SUCCESS, bookArray }
 }
 
 export function loadBooks() {
