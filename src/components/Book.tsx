e34a0ec67f42f6799f379a9884f4baa78ddb33fc
vhetet <vh1030478@gmail.com>
2018-07-25 16:07:55 -0500

    update book model to match the data pulled from the API

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 
 const book = (props:any) => (
     <div className="Book">
-        <p>Book name: {props.book.name}</p>
+        <p>Book Title: {props.book.title}</p>
         <p>Book author: {props.book.author}</p>
-        <p>Book page number: {props.book.page}</p>
+        <p>Book page number: {props.book.pageLength}</p>
         <p>Book price: {props.book.price}</p>
         <button onClick={props.onBookClicked}>Delete</button>
     </div>
--- src/model/book.ts
+++ src/model/book.ts
@@ -1,7 +1,9 @@
 export default interface IBook {
     author: string,
+    coverImage: any,
+    genre: string,
     id: number,
-    name: string,
-    page: number,
-    price: number
+    pageLength: number,
+    price: number,
+    title: string
 }
