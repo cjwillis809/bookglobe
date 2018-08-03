5dc9b5ae0649b65e4471eb064dca38680bb6c22b
cjwillis809 <cjwillis809@gmail.com>
2018-08-03 12:17:18 -0500

    Simplified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,13 @@ import './App.css';
 import BooksList from './components/BooksList';
 import Header from './components/Header/Header';
 
-interface BookAppProps {
-  navigateToLogin: () => void
-}
-
-class App extends React.Component<BookAppProps> {
-  constructor(props: BookAppProps) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <div className="App">
-        <Header />
-        <BooksList />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <BooksList />
+    </div>
+  );
 }
 
 export default App;
